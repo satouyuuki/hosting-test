14d94c7b86d3d51987951724f4c0ac756581b902
satoyuki <yuki01377@gmail.com>
2020-05-03 23:28:05 +0900

    gulp create

---
+++ app/js/main.js
@@ -0,0 +1,45 @@
+$(() => {
+  $(document).ready(() => {
+    const topBtn = $('.top-scroll');
+    const header = $('.header');
+    const spMenu = $('.header__menu-sp');
+    const accordion = $('.accordion');
+    const about = $('#about');
+    $(window).scroll(() => {
+      console.log('scroll');
+      // スクロール位置が最初だったら
+      if ($(this).scrollTop() == 0) {
+        header.removeClass('scroll');
+        return false;
+      } else {
+        header.addClass('scroll');
+      }
+    });
+    //スルスルっとスクロールでトップへもどる
+    topBtn.click(() => {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 500);
+      return false;
+    });
+    //spメニューをクリック
+    spMenu.click(() => {
+      $('.header__menu-sp, .mask, body').toggleClass('active');
+      accordion.slideToggle();
+      if (!header.hasClass('scroll')) {
+        header.addClass('scroll');
+      }
+    });
+    // pcメニューをホバー
+    $('#about, .dropdown').hover(() => {
+      if (!header.hasClass('scroll')) {
+        header.addClass('scroll');
+      }
+      $(".dropdown").stop().slideDown();
+      $('.mask, body').addClass('active');
+    }, function () {
+        $(".dropdown").stop().slideUp();
+        $('.mask, body').removeClass('active');
+    });
+  });
+});
