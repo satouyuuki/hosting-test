5ac5861c759fd63683ca598fdbe38f5d72eeebdd
satoyuki <yuki01377@gmail.com>
2020-05-07 16:37:18 +0900

    basic auth add

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,12 @@ const babel = require("gulp-babel");
 const imagemin = require('gulp-imagemin');
 const mozjpeg = require('imagemin-mozjpeg');
 const pngquant = require('imagemin-pngquant');
+// linter系
+const htmlhint = require("gulp-htmlhint");
+const scsslint = require("gulp-scss-lint");
+const eslint = require("gulp-eslint");
+// const gulpStylelint = require('gulp-stylelint');
+// const reporter = require('postcss-reporter');
 
 const styleSRC = "./app/css/**/*.scss";
 const styleURL = "./dist/css/";
@@ -49,6 +55,32 @@ function browser_sync() {
 
 function reload (done) {
   browserSync.reload();
+  done();
+}
+
+function htmlLint(done) {
+  src(htmlSRC)
+    .pipe(htmlhint())
+    .pipe(htmlhint.reporter())
+  done();
+}
+
+function sassLint(done) {
+  src(styleSRC).pipe(
+    scsslint({
+      config: "scss-lint.yml"
+    })
+  );
+  done();
+}
+
+function esLint(done) {
+  src(jsSRC)
+    .pipe(eslint({
+      fix: true
+    }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
   done();
 }
 
@@ -113,6 +145,10 @@ task("js", js);
 task("images", images);
 task("fonts", fonts);
 task("html", html);
+//lint
+task("html-lint", htmlLint);
+task("sass-lint", sassLint);
+task("eslint", esLint);
 
 task("default", parallel(css, js, images, fonts, html));
 
